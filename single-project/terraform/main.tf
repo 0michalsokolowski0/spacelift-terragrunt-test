039927b7cc79b7c6ab1715e470d97fceaf49a2a1
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-08-28 14:31:12 +0200

    Update main.tf

--- single-project/terraform/main.tf
+++ single-project/terraform/main.tf
@@ -10,11 +10,6 @@ locals {
   labels = ["stack:terraform"]
 }
 
-variable "spacelift_stack_labels" {
-  description = "Stack labels"
-  type        = set(string)
-}
-
 output "labels_output" {
   value = var.spacelift_stack_labels
 } 
